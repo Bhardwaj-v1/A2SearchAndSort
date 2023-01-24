299d0a3990b0e046bd5d3b94649f401e7268e6bc
Varish Bhardwaj <bhardwaj-v@webmail.uwinnipeg.ca>
2023-01-24 11:39:16 -0600

    Bubble and Selection Sort done

--- ArrayFunctions.java
+++ ArrayFunctions.java
@@ -2,12 +2,12 @@
 import java.util.Random;
 
 /* **********************************************************
- * Programmer:	Rob Sveinson
- * Class:		CS20S
+ * Programmer:    Rob Sveinson
+ * Class:        CS20S
  * 
- * Assignment:	A2 Sorting and Searching
+ * Assignment:    A2 Sorting and Searching
  *
- * Description:	class to do some standard array work, load and print to name
+ * Description:    class to do some standard array work, load and print to name
                 two
  **************************************************************/
  
@@ -51,5 +51,52 @@ import java.util.Random;
         
          return actualSize;
      } // end loadLIst
+     
+     
+    public void bubbleSort(int[] list){
+        
+        for (int i = 0; i < list.length - 1; i++){
+            
+            for (int j = 0; j < list.length - i - 1; j++){
+                if (list[j] > list[j + 1]) {
+                    // swap arr[j+1] and arr[j]
+                    final int TEMP = list[j];
+                    list[j] = list[j + 1];
+                    list[j + 1] = TEMP;
+                }
+            }// end for
+        }// end for
+    }// end bubbleSort
+    
+    public void selectionSort(int[] list){
+        
+        
+        for(int i = 0; i < list.length-1; i++){
+        // for finding smallest number
+        for(int j = i + 1; j<list.length; j++){
+        if(list[j] < list[i]){
+         // swapping indexs
+         final int TEMP = list[j];
+         list[j] = list[i];
+         list[i] = TEMP;
+        
+        }// end if
+        }//end for loop
+        
+        }
+    
+    }// end selectionSort
+
+        
+    public void swap(int[] list, int m, int n){
+        String nl = System.lineSeparator();
+        System.out.println(nl +"----- print the array in descend row lengths with sapped elements -----");
+
+        final int TEMP = list[m];
+        list[m] = list[n];
+        list[n] = TEMP;
+
+        }// end of swap
+     
  
  }  // end class
